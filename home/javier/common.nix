90c3ffe62fbcdb7f41d98ac4002a3714407cfc6a
Javier Poremski <javier@poremski.se>
2026-04-13 22:24:36 +0200

    fix(plasma): add standard Dolphin places

--- home/javier/common.nix
+++ home/javier/common.nix
@@ -1,4 +1,12 @@
-_: {
+{config, ...}: {
+  xdg.userDirs = {
+    enable = true;
+    createDirectories = true;
+    setSessionVariables = true;
+  };
+
+  xdg.dataFile."user-places.xbel".source = ../../config/plasma/user-places.xbel;
+
   home = {
     username = "javier";
     homeDirectory = "/home/javier";
